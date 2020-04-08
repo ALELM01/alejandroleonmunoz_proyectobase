2e79d231408b885a8d73c266fc791bb5b5b6f5d9
alejandroleonmunoz1007@gmail.com <alejandroleonmunoz1007@gmail.com>
2020-04-08 16:42:37 +0200

    cambios

---
+++ src/database.js
@@ -0,0 +1,15 @@
+import MongoClient from 'mongodb'
+
+
+export async function connect() {
+    try {
+        const client = await MongoClient.connect('mongodb+srv://user:user@mvico-hbci6.azure.mongodb.net/test?retryWrites=true&w=majority', {
+            useUnifiedTopology: true
+        })
+        const db = client.db('Proyecto')
+        console.log('DB is connected')
+        return db
+    } catch(e) {
+        console.log(e)
+    }
+}
